4cee6a5c48643fc6d2d22cc1553e5564ea1d19c5
Abe <ab.b2k3@gmail.com>
2021-10-17 15:47:26 +0200

    `props` to a Component

---
+++ src/components/AccessComponents.js
@@ -0,0 +1,18 @@
+import React from 'react' //imr + tab
+import ReactDOM from 'react-dom' //imrd + tab
+
+class PropsDisplayer extends React.Component {
+  render() {
+  	const stringProps = JSON.stringify(this.props);
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2>{stringProps}</h2>
+      </div>
+    );
+  }
+}
+
+// ReactDOM.render goes here:
+ReactDOM.render(<PropsDisplayer />, document.getElementById('app'))

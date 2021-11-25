656206e1e9b75fec3afe26de2e9851a022892e12
inspiringsource <ab.b2k3@gmail.com>
2021-11-25 18:16:09 +0100

    Promise(resolve, reject)

---
+++ src/components/advnaced JS/TimeOut.js
@@ -0,0 +1,20 @@
+console.log("This is the first line of code in app.js.");
+// Keep the line above as the first line of code
+// Write your code here:
+
+const usingSTO = () => {
+    console.log("Hello world")
+}
+setTimeout(usingSTO, 2000) // after both console logs, the function will run
+// Output:
+// $ node app.js
+// This is the first line of code in app.js.
+// This is the last line of code in app.js.
+// Hello world
+
+
+
+
+
+// Keep the line below as the last line of code:
+console.log("This is the last line of code in app.js.");
